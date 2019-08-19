729ba82de13915ec96f530a9331f5154d12e23d1
Victor Vuelma <victorvuelma@outlook.com>
2019-08-19 19:27:28 -0300

    add product details structure

--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,13 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Main from './pages/main';
+import Product from './pages/product'
 
 const Routes = () => (
     <BrowserRouter>
         <Switch>
-            <Route path="/" component={Main}></Route>
+            <Route exact path="/" component={Main}></Route>
+            <Route path="/products/:id" component={Product}></Route>
         </Switch>
     </BrowserRouter>
 );
